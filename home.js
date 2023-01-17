dcc65c89490033e3c044d610133b714ebbf53af2
Christiana Bond <101133924+ChristianaBond@users.noreply.github.com>
2023-01-16 22:27:05 -0600

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+      <Card
+      bgcolor="danger"
+      header="Welcom to Bad Bank, Inc."
+      title="Welcome, Bad Bank, Inc. Member!"
+      text="Select the links above to access your account."
+      body={(<img src="bank-icon-5981.png" className="img-fluid" alt="Responsive image"/>)}
+      />
+  );
+}
